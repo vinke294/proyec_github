db8d324df66b93d60d21c66ac0ce2e4a7cb398ce
julio <jhons_aiorushotmail>
2016-07-15 11:34:13 -0500

    calculadora
    programa basico

---
+++ proyecto_github.py
@@ -0,0 +1,20 @@
+print("Programa calculadora")
+
+a=float(input("Ingrese Numero: "))
+b=float(input("Ingrese NUmero: "))
+
+    
+
+def resta(a,b):
+
+    f=a-b
+    print("LA resta es",f)
+
+resta(a,b)
+
+def suma(a,b):
+
+    f=a+b
+    print("La suma es",f)
+
+suma(a,b)
